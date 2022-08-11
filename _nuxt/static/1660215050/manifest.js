ccc9d31fe06c7ec4993847a17d64bb3ca1cdc96a
amalthea-code <amalthea22@yandex.ru>
2022-08-11 14:58:27 +0400

    update v2

--- _nuxt/static/1660215050/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fdoctors","\u002Fnews","\u002Fcontacts","\u002Fpatients","\u002Fprices","\u002F","\u002Fservices","\u002Fabout","\u002Fvacancies"]})
---
+++ _nuxt/static/1660215354/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fdoctors","\u002Fvacancies","\u002Fcontacts","\u002Fservices","\u002Fnews","\u002Fpatients","\u002Fprices"]})
